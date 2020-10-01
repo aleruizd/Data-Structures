5ce1e2e9606d5d528c497df4f5a0b1c06997828d
aleruizdiaz <alejandro.ruizdiaz2000@gmail.com>
2020-09-30 22:19:31 -0300

    Data Structures only with stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MinCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -12,7 +13,13 @@ module.exports = {
     plugins: [
         new MinCssExtractPlugin({
             filename: 'styles.css'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            // injects bundle.js to our new index.html
+            inject: true,
+            // copys the content of the existing index.html to the new /build index.html
+            template:  path.resolve('./public/index.html'),
+          }),
     ],
     module: {
         rules: [
